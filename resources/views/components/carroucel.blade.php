c23a38c26b43fa6cbcc244b6ac6d002a5d1fc8f3
Darken <zerboinoussas4@gmail.com>
2024-07-13 00:15:14 +0000

    => carroucel

--- resources/views/components/carroucel.blade.php
+++ resources/views/components/carroucel.blade.php
@@ -1,39 +1,42 @@
-<section id="demos">
-    <div class="row">
-      <div class="large-12 columns">
-        <div class="owl-carousel owl-theme">
-
-          <div class="item">
-            <img src="{{ asset('images/posts/img_6.jpg') }}" alt="">
-          </div>
-          <div class="item">
-            <img src="{{ asset('images/posts/img_7.jpg') }}" alt="">
-          </div>
-          <div class="item">
-            <img src="{{ asset('images/posts/img_7.jpg') }}" alt="">
-         </div>
-
-          </div>
+ <div id="animation-carousel" class=" relative w-full z-10" data-carousel="static">
+    <!-- Carousel wrapper -->
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+         <!-- Item 1 -->
+        <div class="hidden duration-200 ease-linear" data-carousel-item>
+            <img src="{{ asset('images/posts/img_1.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
-        <script>
-          $(document).ready(function() {
-            var owl = $('.owl-carousel');
-            owl.owlCarousel({
-              items: 1,
-              loop: true,
-              margin: 10,
-              autoplay: true,
-              autoplayTimeout: 3000,
-              autoplayHoverPause: true
-            });
-            $('.play').on('click', function() {
-              owl.trigger('play.owl.autoplay', [3000])
-            })
-            $('.stop').on('click', function() {
-              owl.trigger('stop.owl.autoplay')
-            })
-          })
-        </script>
-      </div>
+        <!-- Item 2 -->
+        <div class="hidden duration-200 ease-linear" data-carousel-item>
+          <img src="{{ asset('images/posts/img_2.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">        
+        </div>
+        <!-- Item 3 -->
+        <div class="hidden duration-200 ease-linear" data-carousel-item="active">
+          <img src="{{ asset('images/posts/img_3.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">        
+          <!-- Item 4 -->
+        <div class="hidden duration-200 ease-linear" data-carousel-item>
+          <img src="{{ asset('images/posts/img_4.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">        
+        </div>
+        <!-- Item 5 -->
+        <div class="hidden duration-200 ease-linear" data-carousel-item>
+          <img src="{{ asset('images/posts/img_5.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">        
+        </div>
     </div>
-  </section>
+    <!-- Slider controls -->
+    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span class="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+            </svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
+</div>
+
